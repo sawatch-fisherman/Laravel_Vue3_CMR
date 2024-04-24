4ebf274364616d1b0ee935373b9add547b6ebc01
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-04-25 05:35:40 +0900

    40. Item 下準備2: モデル・ルーティング 41. Item 下準備3: ユーザーシーダー、ログイン後のロゴ調整 42. ItemsSeeder、ナビメニュー

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use App\Http\Controllers\InertiaTestController;
 |
 */
 
+// CRUDを一括で作成
+Route::resource('items', ItemController::class)->middleware(['auth', 'verified']);
+
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
     }
@@ -26,7 +30,6 @@ Route::get('/component-test', function(){
     return Inertia::render('ComponentTest');
     }
 );
-
 
 Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('inertia.create');
