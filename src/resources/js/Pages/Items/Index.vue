9156b287f99811db9270dabee6974c453882abf4
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-04-27 06:33:13 +0900

    50. Item フラッシュメッセージ

--- src/resources/js/Pages/Items/Index.vue
+++ src/resources/js/Pages/Items/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import FlashMessage from '@/Components/FlashMessage.vue'
 
 defineProps({
     items: Array
@@ -22,6 +23,8 @@ defineProps({
                     <div class="p-6 text-gray-900">
                         <section class="text-gray-600 body-font">
                             <div class="container px-5 py-8 mx-auto">
+
+                                <FlashMessage />
 
                                 <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
                                     <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
