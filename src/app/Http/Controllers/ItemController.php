5d99204b0fce6db6803a1f13422ca52e15b47ae6
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-04-28 23:31:10 +0900

    - 53. Item Edit 商品編集その1 - 54. Item Edit 商品編集その2

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -76,7 +76,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render('Items/Edit', [
+            'item' => $item
+        ]);
     }
 
     /**
