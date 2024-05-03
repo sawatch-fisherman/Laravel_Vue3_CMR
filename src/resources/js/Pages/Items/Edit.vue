aaf2056ae2294e88202ee0b0271f84589e2099b7
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-03 14:19:21 +0900

    - 65. Customer 新規登録

--- src/resources/js/Pages/Items/Edit.vue
+++ src/resources/js/Pages/Items/Edit.vue
@@ -61,6 +61,7 @@ const updateItem = id => {
                                                     <input type="number" id="price" name="price" v-model="form.price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 </div>
                                             </div>
+
                                             <div class="p-2 w-full">
                                                 <div class="relative">
                                                     <label for="is_selling" class="leading-7 text-sm text-gray-600">ステータス</label>
@@ -70,6 +71,7 @@ const updateItem = id => {
                                                     <label class="ml-2 mr-4">停止中</label>
                                                 </div>
                                             </div>
+
                                             <div class="p-2 w-full">
                                                 <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                             </div>
