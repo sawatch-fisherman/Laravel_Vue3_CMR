5fea7184e77f59d73e644e01a34c377ef29d2d1b
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-05 17:27:58 +0900

    - 72. 購入日の日付を取得(JS Dateオブジェクト)

--- src/resources/js/common.js
+++ src/resources/js/common.js
@@ -5,5 +5,13 @@ const nl2br = (str) => {
     return res;
 }
 
+const getToday = () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = ("0" + (today.getMonth() + 1)).slice(-2);
+    const dd = ("0" + today.getDate()).slice(-2);
+    return yyyy + '-' + mm + '-' + dd;
+}
+
 // 外部から呼び出し可能にする
-export { nl2br }
+export { nl2br, getToday }
