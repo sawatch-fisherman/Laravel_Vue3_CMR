7a30fc39435361b30c0f6d25c22dfcbf22f8a000
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-06 13:56:43 +0900

    - 74. 合計金額をcomputedで計算

--- src/resources/js/Pages/Purchases/Create.vue
+++ src/resources/js/Pages/Purchases/Create.vue
@@ -1,19 +1,32 @@
 <script setup>
 import { getToday } from '@/common';
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 
 const props = defineProps({
-    'customers': Array
+    'customers': Array,
+    'items': Array
 })
 
 onMounted(() => {
     form.date = getToday()
+    props.items.forEach(item => {
+        itemList.value.push({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            quantity: 0
+        })
+    })
 })
+
+const itemList = ref([])
 
 const form = reactive({
     date: null,
     customer_id: null
 })
+
+const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] // option用
 
 </script>
 
@@ -27,4 +40,33 @@ const form = reactive({
             {{ customer.id }} : {{ customer.name }}
         </option>
     </select>
+    <br><br>
+
+    商品・サービス<br>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>商品名</th>
+                <th>金額</th>
+                <th>数量</th>
+                <th>小計</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in itemList">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td>
+                    <select name="quantity" v-model="item.quantity">
+                        <option v-for="q in quantity" :value="q">{{ q }}</option>
+                    </select>
+                </td>
+                <td>
+                    {{ item.price * item.quantity }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
