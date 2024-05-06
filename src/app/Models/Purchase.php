cfbfe0910cd833c869cbcf317bef33c63e0a24bb
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-06 15:22:35 +0900

    - 76. 保存処理その1 POST通信 - 77. 保存処理その2 DBに保存

--- src/app/Models/Purchase.php
+++ src/app/Models/Purchase.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Customer;
-use app\Models\Item;
+use App\Models\Item;
 
 class Purchase extends Model
 {
