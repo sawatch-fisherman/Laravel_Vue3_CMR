246ead5a965878c022b547fe8c03ba253614657f
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-06 11:17:07 +0900

    - 73. 顧客情報をv-forで表示

--- src/resources/js/Pages/Purchases/Create.vue
+++ src/resources/js/Pages/Purchases/Create.vue
@@ -2,16 +2,29 @@
 import { getToday } from '@/common';
 import { onMounted, reactive } from 'vue';
 
+const props = defineProps({
+    'customers': Array
+})
+
 onMounted(() => {
     form.date = getToday()
 })
 
 const form = reactive({
-    date: null
+    date: null,
+    customer_id: null
 })
+
 </script>
 
 <template>
-日付<br>
-<input type="date" name="date" v-model="form.date">
+    日付<br>
+    <input type="date" name="date" v-model="form.date">
+
+    会員名<br>
+    <select name="customer" v-model="form.customer_id">
+        <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+            {{ customer.id }} : {{ customer.name }}
+        </option>
+    </select>
 </template>
