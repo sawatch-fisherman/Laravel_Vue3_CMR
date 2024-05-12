fba15fe92d1b6eaaf104c755ecf4dffab619e90c
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-13 05:58:48 +0900

    - 90. 購買履歴の詳細画面(show)(小計・合計)

--- src/resources/js/Pages/Purchases/Index.vue
+++ src/resources/js/Pages/Purchases/Index.vue
@@ -66,7 +66,10 @@ onMounted(() => {
                                         <tbody>
                                             <tr v-for="order in props.orders.data" :key="order.id">
                                                 <td class="border-b-2 border-gray-200 px-4 py-3">
-                                                    {{ order.id }}
+                                                    <Link class="text-blue-400"
+                                                        :href="route('purchases.show', { purchase: order.id })">
+                                                        {{ order.id }}
+                                                    </Link>
                                                 </td>
                                                 <td class="border-b-2 border-gray-200 px-4 py-3">{{ order.customer_name }}
                                                 </td>
