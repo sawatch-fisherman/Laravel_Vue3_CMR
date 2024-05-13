e34cd1d10633db4de639055ea7761ed3e6208092
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-14 06:54:49 +0900

    - 92. 購買履歴の編集画面(edit)の準備 - 93. 購買履歴の編集用コード(Laravel側)

--- src/resources/js/Pages/Purchases/Show.vue
+++ src/resources/js/Pages/Purchases/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import { onMounted, reactive, ref, computed } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
@@ -124,7 +124,8 @@ onMounted(() => {
                                             </div>
 
                                             <div class="p-2 w-full">
-                                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                                <!-- <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button> -->
+                                                <Link as="button" :href="route('purchases.edit', { purchase: props.order[0].id })" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
                                             </div>
 
                                         </div>
