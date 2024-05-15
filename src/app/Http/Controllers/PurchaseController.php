995f13655463a92563ac9b27d57cf28da2cb8ee9
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-16 06:08:01 +0900

    - 95. 購買履歴の更新その1 - 96. 購買履歴の更新その2(sync)

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -150,7 +150,29 @@ class PurchaseController extends Controller
      */
     public function update(UpdatePurchaseRequest $request, Purchase $purchase)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $purchase->status = $request->status;
+            $purchase->save();
+
+            $items = [];
+
+            foreach($request->items as $item){
+                $items = $items + [
+                    $item['id'] => [
+                        'quantity' => $item['quantity']
+                    ]
+                ];
+            }
+
+            $purchase->items()->sync($items);
+
+            DB::commit();
+            return to_route('dashboard');
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
     }
 
     /**
