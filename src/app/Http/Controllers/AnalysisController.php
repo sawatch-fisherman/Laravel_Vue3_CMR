4d83d1a87edfda86ce756e7be34f217c571d6d5f
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-17 06:06:18 +0900

    - 98. 期間を指定する

--- src/app/Http/Controllers/AnalysisController.php
+++ src/app/Http/Controllers/AnalysisController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Order;
 
 class AnalysisController extends Controller
 {
     public function index()
     {
+        $startDate = '2024-04-10';
+        $endDate = '2024-05-17';
+
+        $period = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
+        ->orderBy('created_at')
+        ->paginate(50);
+
+        dd($period);
+
         return Inertia::render('Analysis');
     }
 }
