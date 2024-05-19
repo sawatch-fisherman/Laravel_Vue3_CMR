aeef4d6698c22700fb3dad717f2c235011fced6d
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-05-19 12:55:24 +0900

    - 103. 日別売上のテーブル表示

--- src/resources/js/Pages/Analysis.vue
+++ src/resources/js/Pages/Analysis.vue
@@ -15,6 +15,8 @@ const form = reactive({
     type: 'perDay'
 })
 
+const data = reactive({})
+
 const getData = async () => {
     try {
         await axios.get('/api/analysis/', {
@@ -25,7 +27,7 @@ const getData = async () => {
             }
         })
             .then(res => {
-                // data.value = res.data
+                data.data = res.data.data
                 console.log(res.data)
             })
     } catch (e) {
@@ -35,6 +37,7 @@ const getData = async () => {
 </script>
 
 <template>
+
     <Head title="データ分析" />
 
     <AuthenticatedLayout>
@@ -51,8 +54,31 @@ const getData = async () => {
                         <form @submit.prevent="getData">
                             From: <input type="date" name="startDate" v-model="form.startDate">
                             To: <input type="date" name="endDate" v-model="form.endDate"><br>
-                            <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                            <button
+                                class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
                         </form>
+
+                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
+                            <table class="table-auto w-full text-left whitespace-no-wrap">
+                                <thead>
+                                    <tr>
+                                        <th
+                                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                            年月日</th>
+                                        <th
+                                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                            金額</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="item in data.data" :key="item.date">
+                                        <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
+                                        <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
             </div>
